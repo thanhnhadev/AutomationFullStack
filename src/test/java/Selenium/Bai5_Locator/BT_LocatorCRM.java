f89db014faf7e7d3e313876138cffee3092d879e
thanhnha_test <thanhnhadev@gmail.com>
2024-03-25 11:04:21 +0700

    Selenium_Locator

---
+++ src/test/java/Selenium/Bai5_Locator/BT_LocatorCRM.java
@@ -0,0 +1,26 @@
+package Selenium.Bai5_Locator;
+
+public class BT_LocatorCRM {
+    public static String headerLoginPage ="//h1[contains(.,'Login')]";
+    public static String txtEmail="//input[@id='email']";
+    public static String txtPassword = "//input[@id='password']";
+    public static String btnLogin = "//button[contains(.,'Login')]";
+    public  static String linkForGotPassword = "//a[contains(.,'Forgot Password?')]";
+    //Dashboard
+    public static String menuCustomers="//span[contains(.,'Customers')]";
+    public static String menuProject="//span[contains(.,'Projects')][1]";
+    public static String menuTask="//span[contains(.,'Task')][1]";
+    public static String menuDashboard ="//span[contains(.,'Dashboard')]";
+    //Customers
+    public static String btnAddNewCustomer = "//a[contains(@href,'New Customer')]";
+    public static String  btnImportCustomer="//a[contains(@href,'Import Customers')]";
+    public static String  btnContack="//a[contains(@href,'all_contacts')]";
+    public static String  headerCustomerPage="//span[contains(.,'Customers Summary')]";
+    public static String  abc="";
+    public static String  abc="";
+    public static String  abc="";
+    public static String  abc="";
+
+
+
+}
